90bdcb5e4132b6142fd4bfdfa4a7beb2970e7152
EwaDwornik <ewaannadwornik@gmail.com>
2021-11-29 09:04:03 +0000

    + requirements

--- NoFuture/settings.py
+++ NoFuture/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
+import sys
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 
